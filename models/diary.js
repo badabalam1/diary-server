11cf293f6347876a30dd36e4a59cb8fa19a283f9
baehyeonbin <baehynebin@hotmail.co.kr>
2017-12-05 09:51:14 +0900

    Initial commit

---
+++ models/diary.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var diarySchema = new mongoose.Schema({
+    title : {type : String, required: true},
+    content : {type :String, required : true},
+    date : {type : Date, default : Date.now}
+});
+
+module.exports = mongoose.model('diary', diarySchema);
---
+++ routes/diaries.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var Diary = require('../models/diary');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    Diary.find({}, (err, diaries) => {
+        if(err)
+            res.status(200).json({result : {success : false , message : err.message}});
+        res.json({diaries : diaries, result : {success : true, message : '조회에 성공하였습니다.' }});
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Diary.findOne({_id : req.params.id}, (err, diary) => {
+        if(err)
+            res.status(200).json({result : {success : false, message :  err.message}});
+        res.json({diary : diary, result : {success : true, message :  '성공적으로 조회되었습니다!'}});
+    });
+});
+
+router.post('/', (req, res) => {
+    Diary.create(req.body, (err, post) => {
+        if(err)
+            res.status(200).json({result : {success : false, message : err.message}});
+        console.log(post);
+        res.json({result : {success : true, message : '성공적으로 작성되었습니다.'}});
+    });
+});
+
+router.put('/:id', (req, res) => {
+    var id = req.params.id;
+    Diary.update({_id : id}, req.body, (err, raw) => {
+        if(err)
+            res.status(200).json({result : {success : false, message : err.message}});
+        res.json({result : {success : true, message : '성공적으로 수정이 완료되었습니다.'}});
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    var id = req.params.id;
+    Diary.remove({id : req.params.id}, (err) => {
+        if(err)
+            res.json({result : {success : false, message : err.message}});
+        res.json({result : {success : true, message : '성공적으로 삭제되었습니다!'}});
+    });
+});
+
+module.exports = router;
