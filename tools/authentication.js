979a0511bf9edcb26a5621ee64c8d660d19c25a5
baehynebin <baehynebin@hotmail.co.kr>
2017-12-13 19:31:04 +0900

    로그 추가

--- tools/authentication.js
+++ tools/authentication.js
@@ -1,7 +1,7 @@
 var jwt = require('jsonwebtoken');
 var config = require('../configs/config');
 module.exports = (req, res, next) => {
-    console.log("header : " + req.headers);
+    console.log("header : " + req.headers.toString());
     if(req.headers.authorization) {
         jwt.verify(req.headers.authorization, config.salt , (err, decoded) => {
             if (!err && decoded) {
