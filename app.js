64c0f463e29c09b0a0c8f5955093de4d69ce4774
배현빈 <baehyeonbin@hotmail.co.kr>
2018-06-03 05:17:40 +0900

    validator fucking

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(file());
 app.use(authentication);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
 app.use('/', index);
 app.use('/diaries', diaries);
 app.use('/users/', users);
@@ -43,12 +50,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
